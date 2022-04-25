b75192c79049eef0ca5a65e011315fa16a237e23
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-04-25 12:07:44 +0300

    ✨ feat: MySQL password expiration

---
+++ password_expiration.sql
@@ -0,0 +1,5 @@
+-- ALTER USER 'tempuser'@'localhost' PASSWORD EXPIRE;
+-- ALTER USER 'tempuser'@'localhost' PASSWORD EXPIRE INTERVAL 90 DAY;
+-- ALTER USER 'tempuser'@'localhost' PASSWORD EXPIRE NEVER;
+-- ALTER USER 'tempuser'@'localhost' PASSWORD EXPIRE DEFAULT;
+-- DROP USER 'tempuser'@'localhost';
